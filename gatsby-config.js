d9d4944c33910c79f246565c4d6996f00e6c5247
Hieu Nguyen <hieunguyenel@gmail.com>
2021-08-29 11:55:06 +0700

    blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,14 +68,6 @@ module.exports = {
         icon: `src/assets/img/portfolio-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-source-github-api`,
-      options: {
-        token: process.env.GATSBY_GITHUB_API_TOKEN,
-        graphQLQuery: data.githubApiQuery,
-        variables: data.githubApiVariables,
-      },
-    },
     // https://www.gatsbyjs.org/packages/gatsby-plugin-google-analytics/
     {
       resolve: `gatsby-plugin-google-analytics`,
